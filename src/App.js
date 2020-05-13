147f56d32a92226cef4baf1a32473abd72cf0063
matidiaz032 <maty_032@hotmail.es>
2020-05-13 00:08:28 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bulma/css/bulma.css'
+import { Title } from './components/Title'
+
+function App() {
+  return (
+    <div className="App">
+      <Title>Search Movies</Title>
+    </div>
+  );
+}
+
+export default App;
